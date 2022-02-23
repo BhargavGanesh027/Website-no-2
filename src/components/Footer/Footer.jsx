3d3dbc0010d81bee30c803726a3e3409a24cfffa
bhargav <ganeshbhargav027@gmail.com>
2022-02-23 15:39:19 +0530

    footer is completed
    i have added the footer and links that navigate to other pages
    we will see what happens now
    woohooo
    :wq

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './Footer.css'
+import { AiFillLinkedin, AiOutlineGithub } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>2022 All Rights Reserved </h1>
-
+    <div className='app-footer'>
+      <h1 className='app-footer-header'>2022 All Rights Reserved </h1>
+     <button type='button' onClick={Event => window.location.href='https://www.linkedin.com/in/ganesh-chowdary-910b71195/'}>
+       <AiFillLinkedin className='linkedin-logo'/>
+      </button>
+      <button type='button' onClick={Event => window.location.href='https://www.linkedin.com/in/ganesh-chowdary-910b71195/'}>
+      <AiOutlineGithub className='github-logo'/>
+      </button>
     </div>
   )
 }
